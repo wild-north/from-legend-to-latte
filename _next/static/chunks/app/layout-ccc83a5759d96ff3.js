26860d7443e07a2e29898dd11115a72e8ab76209
wild-north <wild-north@users.noreply.github.com>
2025-07-21 09:55:54 +0000

    deploy: e37358ed07ae64bbf8fe69835df2c5423830608f

---
+++ _next/static/chunks/app/layout-ccc83a5759d96ff3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{347:()=>{},1240:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,6874,23)),Promise.resolve().then(n.t.bind(n,347,23))}},e=>{e.O(0,[690,874,441,964,358],()=>e(e.s=1240)),_N_E=e.O()}]);
